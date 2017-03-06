0f4215e3b08362a8b66359f59648934cd45aa83e
Karim C <karim.cheurfi@gmail.com>
2017-03-06 19:42:12 +0100

    add muttrc and update fish config

--- config.fish
+++ config.fish
@@ -13,7 +13,11 @@ alias tmux="tmux -u"
 alias activate="source env/bin/activate"
 alias wintermute="ssh zabana@163.172.134.19"
 
+# Change caps lock to control
+setxkbmap -option caps:ctrl_modifier
+
 # Env vars
 
 set -x GOPATH $HOME/gowork
 set -x PATH $GOPATH/bin $PATH
+set -x EDITOR /usr/bin/vim
