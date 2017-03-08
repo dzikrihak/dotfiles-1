54ca81c5d4cf8f5803718aab78a5a7a12736033f
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-03-08 20:47:12 +0000

    remove setxkbmap script

--- config.fish
+++ config.fish
@@ -13,9 +13,6 @@ alias tmux="tmux -u"
 alias activate="source env/bin/activate"
 alias wintermute="ssh zabana@163.172.134.19"
 
-# Change caps lock to control
-setxkbmap -option caps:ctrl_modifier
-
 # Env vars
 
 set -x GOPATH $HOME/gowork
